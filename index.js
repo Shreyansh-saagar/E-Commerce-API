b474fc0046c66c9df798bbf11459238d850aa61b
shreyansh saagar <shreyanshsaagar@gmail.com>
2024-02-27 01:13:04 +0530

    CORS policy config done and 404 request Handeled

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ import basicAuth from './src/middlewares/basic-auth.middleware.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import cartrouter from './src/features/cart/cart.routes.js';
 import apidocs from './swagger.json' assert {type:'json'}
-
+import cors from 'cors'
 
 const PORT = '5100';
 const app = express();
+
+
+// Handling CORS manually config -> * to allow eveything else to restrict you can specify it by seprating with ','
+/* app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers','*')
+    res.header('Access-Control-Allow-Methods', '*')
+
+    if(req.method == "OPTIONS"){
+        return res.sendStatus(200)
+    }
+
+    next()
+}) */
+
+
+// Handling CORS using library
+app.use(cors())
+
 
 // Application level middlewares
 app.use(bodyParser.json())
@@ -34,6 +53,13 @@ app.use('/api/cart', jwtAuth, cartrouter)
 app.get('/', (req, res) => {
     res.send("Welcome to E-com API")
 })
+
+// Middleware to handle all the requests which doesn't exist to handle 404
+// always keep it in end
+app.use((req,res)=>{
+    res.status(404).send("API not found")
+})
+
 
 app.listen(PORT,()=>{
     console.log(`App listening on port: ${PORT}`);
